5ebdcb7ab2a950e42e91fd773e04f8dc3b951fe0
imsuraj018 <surajkatkar018@gmail.com>
2026-05-10 15:21:55 +0530

    Rest part of 5th practicle

---
+++ PS5_Prism.cpp
@@ -0,0 +1,95 @@
+// Prim's Minimum Spanning Tree Algorithm
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+#define V 5
+
+// Function to find minimum key vertex
+int minKey(int key[], bool mstSet[])
+{
+    int min = INT_MAX;
+    int minIndex;
+
+    for (int v = 0; v < V; v++)
+    {
+        if (!mstSet[v] && key[v] < min)
+        {
+            min = key[v];
+            minIndex = v;
+        }
+    }
+
+    return minIndex;
+}
+
+// Function to print MST
+void printMST(int parent[], int graph[V][V])
+{
+    cout << "Edge \tWeight\n";
+
+    for (int i = 1; i < V; i++)
+    {
+        cout << parent[i] << " - " << i
+             << " \t" << graph[i][parent[i]] << endl;
+    }
+}
+
+// Prim's Algorithm
+void primMST(int graph[V][V])
+{
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+
+    // Initialize all keys as infinite
+    for (int i = 0; i < V; i++)
+    {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
+    }
+
+    // Start from first vertex
+    key[0] = 0;
+    parent[0] = -1;
+
+    // MST will have V vertices
+    for (int count = 0; count < V - 1; count++)
+    {
+        // Pick minimum key vertex
+        int u = minKey(key, mstSet);
+
+        mstSet[u] = true;
+
+        // Update adjacent vertices
+        for (int v = 0; v < V; v++)
+        {
+            if (graph[u][v] &&
+                !mstSet[v] &&
+                graph[u][v] < key[v])
+            {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, graph);
+}
+
+int main()
+{
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+
+    primMST(graph);
+
+    return 0;
+}
